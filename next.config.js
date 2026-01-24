a2dfc44aff6592924cdc42e8231e3f87fe8a00c3
amzn7285 <amzn7285@gmail.com>
2026-01-24 19:54:08 +0530

    Update next.config.js export and remove warnings
    Changed export syntax from ES6 to CommonJS and removed warnings for ignoring build errors.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // Use temporarily to unblock deployment — fix the real TS/tsconfig issue later!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // !! WARN !!
-    // Allows production builds even with ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
